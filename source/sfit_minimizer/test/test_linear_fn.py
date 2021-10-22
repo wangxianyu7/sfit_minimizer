629df57d2b8d6f110c4de1f63553f238487aa945
jenniferyee <jennifer.c.yee@gmail.com>
2021-10-22 10:51:03 -0400

    LinearFunction class complete. 1 very basic test written.

--- source/sfit_minimizer/test/test_linear_fn.py
+++ source/sfit_minimizer/test/test_linear_fn.py
@@ -41,7 +41,7 @@ class LinearFunction():
         self._sigmas = None
         self._step = None
 
-    def update_all(self, theta0):
+    def update_all(self, theta0=None):
         """
         For a new set of model parameters theta0, recalculate all of the
         data properties with respect to the new model.
@@ -49,8 +49,10 @@ class LinearFunction():
         :param theta0:
         :return:
         """
-        self.theta = theta0
-        self._reset_all()
+        if theta0 is not None:
+            self._reset_all()
+            self.theta = theta0
+
         self.calc_model()
         self.calc_res()
         self.calc_chi2()
@@ -61,36 +63,128 @@ class LinearFunction():
         self.calc_cmat()
         self.calc_step()
 
+    # Calculation functions
     def calc_model(self):
-        pass
+        """Calculate expected values of the model"""
+        ymod = []
+        for i in range(len(self.theta)):
+            ymod.append(self.theta[i] * self.data[:, 0] ** i)
+
+        ymod = np.array(ymod)
+        #print('calc_model(): shape of ymod (expect len(theta) x N)', ymod.shape)
+        self._ymod = np.sum(ymod, axis=0)
+        #print('calc_model(): shape of ymod after sum (expect N)', self.ymod.shape)
 
     def calc_res(self):
-        pass
+        if self.ymod is None:
+            self.calc_model()
+
+        self._res = self.ymod - self.data[:, 1]
 
     def calc_chi2(self):
-        pass
+        if self.res is None:
+            self.calc_res()
+
+        self._chi2 = np.sum(self.res ** 2 / self.data[:, 2] ** 2)
 
     def calc_df(self):
-        pass
+        """Calculate the derivatives of the fitting function and store as self.df."""
+        df = []
+        for i in range(len(self.theta)):
+            df.append(self.data[:, 0] ** i)
+
+        self._df = np.array(df)
+        #print('calc_df(): shape of df (expect len(theta) x N)', self.df.shape)
 
     def calc_dchi2(self):
-        pass
+        """Calculate the gradient of chi2 at each datapoint"""
+        if self.df is None:
+            self.calc_df()
+
+        if self.res is None:
+            self.calc_res()
+
+        chi2_gradient = []
+        for i in range(len(self.theta)):
+            chi2_gradient.append(
+                -2 * self.res * self.df[i] / self.data[:, 2]**2)
+
+        self._dchi2 = np.array(chi2_gradient)
+        #print(
+        #    'calc_dchi2(): shape of dchi2 (expect len(theta) x N)',
+        #    self.dchi2.shape)
+
+    def calc_chi2_gradient(self):
+        """Calculate the gradient of chi2 summed over ALL datapoints"""
+        if self.dchi2 is None:
+            self.calc_dchi2()
+
+        self._chi2_gradient = np.sum(self.dchi2, axis=1)
+        #print(
+        #    'calc_chi2_gradient(): shape of chi2_gradient, expect len(theta)',
+        #    self.chi2_gradient.shape)
 
     def calc_dvec(self):
-        pass
+        """
+        Calculate the d vector and store it as self.dvec.
+        d_i = d_i + (partial chi2/partial a_i)/2
+        """
+        if self.dchi2 is None:
+            self.calc_dchi2()
+
+        self._dvec = np.sum(self.dchi2, axis=1) / 2.
+        #print('calc_dvec(): shape of dvec, expect len(theta)', self.dvec.shape)
 
     def calc_bmat(self):
-        pass
+        """
+        Calculate the b matrix and store it as self.bmat.
+        b_ij = b_ij + (partial F/partial a_i)(partial F/partial a_j)/error^2
+        """
+        if self.df is None:
+            self.calc_df()
+
+        self._bmat = np.zeros((len(self.theta), len(self.theta)))
+        for i in range(len(self.theta)):
+            for j in range(len(self.theta)):
+                self._bmat[i, j] = np.sum(
+                    self.df[i] * self.df[j] / self.data[:, 2]**2)
 
     def calc_cmat(self):
-        pass
+        """Invert the b matrix to find the c matrix."""
+        if self.bmat is None:
+            self.calc_bmat()
+
+        self._cmat = np.invert(self.bmat)
 
     def calc_step(self):
-        pass
+        """
+        Calculate the full proposed step size.
+        Delta a_i = sum_j c_ij * d_j.
+        """
+        if self.cmat is None:
+            self.calc_cmat()
+
+        if self.dvec is None:
+            self.calc_dvec()
+
+        self._step = np.zeros(len(self.theta))
+        for i in range(len(self.theta)):
+            for j in range(len(self.theta)):
+                self._step[i] += self.cmat[i, j] * self.dvec[j]
 
     def calc_sigmas(self):
-        pass
+        """
+        Calculate the uncertainty in each parameter theta.
+        sigma_a_i = sqrt(c_ii)
+        """
+        if self.cmat is None:
+            self.calc_cmat()
 
+        self._sigmas = np.zeros(len(self.theta))
+        for i in range(len(self.theta)):
+            self._sigmas[i] = np.sqrt(self.cmat[i, i])
+
+    # Properties
     @property
     def ymod(self):
         """ model values for data[:, 0]. """
@@ -150,9 +244,6 @@ class LinearFunction():
     @property
     def sigmas(self):
         """Uncertainty in each parameter theta"""
-        if self._sigmas is None:
-            self.calc_sigmas()
-
         return self._sigmas
 
     @property
@@ -160,62 +251,73 @@ class LinearFunction():
         """Step for each parameter theta"""
         return self._step
 
-    # Chi2 of the line
+    # Retrieval Functions
     def get_chi2(self, theta):
-        ymod = []
-        for i in range(len(theta)):
-            ymod.append(theta[i] * self.data[:, 0] ** i)
-
-        ymod = np.array(ymod)
-        print(ymod.shape)
-        ymod = np.sum(ymod, axis=1)
-        print(ymod.shape)
-
-        self.res = ymod - self.data[:, 1]
-        self.chi2 = np.sum(self.res ** 2 / self.data[:, 2] ** 2)
+        """Calculate and return the chi2 of the data w.r.t. the model"""
+        self.calc_chi2()
 
         return self.chi2
 
-    # Derivative of a line
-    def get_partials(self, theta):
-        df = []
-        for i in range(len(theta)):
-            df.append(self.data[:, 0] ** i)
 
-        self.df = np.array(df)
-        print(self.df.shape)
+    def get_partials(self, theta):
+        """Calculate and return the partial derivatives of the fitting function"""
+        self.calc_df()
 
         return np.sum(self.df, axis=1)
 
-    # Derivative of the chi2 of a line
     def get_chi2_gradient(self, theta):
-        chi2_gradient = []
-        for i in range(len(theta)):
-            chi2_gradient.append(
-                -2 * self.res * self.df[i] / self.data[:, 2] ** 2)
-
-        self.chi2_gradient = np.array(chi2_gradient)
-        print(self.chi2_gradient.shape)
+        """Calculate and return the derivative of the chi2 of a line."""
+        self.calc_chi2_gradient()
 
         return np.sum(self.chi2_gradient, axis=1)
 
     def get_dvec(self):
-        self.dvec = np.sum(self.chi2_gradient, axis=0) / 2.
-        print(self.dvec.shape)
+        """Calculate and return the d vector"""
+        self.calc_dvec()
 
         return self.dvec
 
     def get_bmat(self):
-        self.bmat = np.zeros((len(self.theta), len(self.theta)))
+        """Calculate and return the b matrix"""
+        self.calc_bmat()
 
+        return self.bmat
+
+    def get_cmat(self):
+        """Calculate and return the c matrix"""
+        self.calc_cmat()
+
+        return self.cmat
+
+    def get_step(self):
+        """Calculate and return the full step for theta_new = theta_old + step."""
+        self.calc_step()
+
+        return self.step
+
+    def get_sigmas(self):
+        """Calculate and return the uncertainty in each parameter theta."""
+        self.calc_sigmas()
+
+        return self.sigmas
+
+
+def test_perfect_chi2():
+    """chi2 should be 0"""
+    data = np.loadtxt('test_data_10perfect.txt', skiprows=2)
+    theta = [3, 2]
+    my_func = LinearFunction(data=data, theta=theta)
+    my_func.update_all()
+
+    tol = 1e-6
+    assert np.sum(my_func.res) < tol
+    assert my_func.chi2 < tol
 
 def test_1():
     """
     Fit a line to perfect data: y = m * x + b
     :return:
     """
-
-
     # Fake data
 
     # Wrong initial condition
@@ -223,3 +325,19 @@ def test_1():
     # Compare results
     # Check b, c, d matrices
     # Check true parameters are recovered.
+
+def test_2():
+    """
+    Fit a line to imperfect data: y = m * x + b
+    :return:
+    """
+    # Fake data
+
+    # Wrong initial condition
+
+    # Compare results
+    # Check b, c, d matrices
+    # Check true parameters are recovered.
+
+if __name__ == '__main__':
+    test_perfect_chi2()

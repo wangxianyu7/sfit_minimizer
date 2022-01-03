07b97a48aab686be028ae4dee8ad48316e967cc0
jenniferyee <jennifer.c.yee@gmail.com>
2022-01-03 16:29:21 -0500

    Reorganized to improve documentation (also updated documentation).

--- source/sfit_minimizer/sfit_classes.py
+++ source/sfit_minimizer/sfit_classes.py
@@ -17,17 +17,19 @@ class SFitResults(object):
 
     """
 
-    def __init__(self, func, success=None, msg=None):
+    def __init__(self, func, success=None, msg=None, iterations=None):
         self._x = func.theta
         self._sigmas = func.get_sigmas()
         self._success = success
         self._msg = msg
+        self._iter = iterations
 
     def __repr__(self):
         output_str = 'x = {0}\n'.format(self.x)
         output_str += 'sigmas = {0}\n'.format(self.sigmas)
         output_str += 'success = {0}\n'.format(self.success)
-        output_str += 'msg = {0}'.format(self.msg)
+        output_str += 'msg = {0}\n'.format(self.msg)
+        output_str += 'iter = {0}'.format(self.iterations)
         return output_str
 
     @property
@@ -66,6 +68,14 @@ class SFitResults(object):
     def msg(self, value):
         self._msg = value
 
+    @property
+    def iterations(self):
+        """ Total number of iterations executed."""
+        return self._iter
+
+    @iterations.setter
+    def iterations(self, value):
+        self._iter = value
 
 class SFitFunction(object):
     """
@@ -114,10 +124,39 @@ class SFitFunction(object):
         self._sigmas = None
         self._step = None
 
+    # theta
+    @property
+    def theta(self):
+        """
+        *list* or *np.array*
+
+        Parameters of the function being fit to the data. Setting this parameter will reset all other parameters to
+        None. Use :py:func:`~update_all()` to update everything.
+        """
+        return self._theta
+
+    @theta.setter
+    def theta(self, value):
+        self._reset_all()
+        if value is None:
+            self._theta = val0ue
+        else:
+            if isinstance(value, (list, np.ndarray)):
+                self._theta = value
+            else:
+                raise TypeError(
+                    'theta must be either list or np.array. Type: {0}'.format(type(value))
+                )
+
     def update_all(self, theta0=None):
         """
-        For a new set of model parameters theta0, recalculate all of the
-        data properties with respect to the new model.
+        Recalculate all of the data properties with respect to the new model parameters.
+
+        Keywords:
+            theta0: *list* of length M, optional
+                new trial values for the parameters of the function to be fit. If not provided, recalculate using
+                the current value of theta.
+
         """
         if theta0 is not None:
             self._reset_all()
@@ -133,167 +172,11 @@ class SFitFunction(object):
         self.calc_cmat()
         self.calc_step()
 
-    # Calculation functions
-    def calc_model(self):
-        """
-        FUNCTION SPECIFIC: Calculate expected values of the model. May
-        be explicitly defined for a specific class that inherits
-        SFitFunction.
-
-        sets self.ymod =
-            *np.array* of shape (N), where each element k is the value of the model evaluated at data[k, 0].
-
-        """
-        raise NotImplementedError('User must define calc_model() for a child class of SFitFunction.')
-
-    def calc_residuals(self):
-        """
-        Difference between the data and the model. Either this or
-        calc_model() should be explicitly defined for a specific
-        class that inherits SFitFunction.
-
-        sets self.residuals =
-            *np.array* of shape (N), where each element k = model[k] - data[k, 1].
-
-        """
-        if self.ymod is None:
-            self.calc_model()
-
-        self.residuals = self.ymod - self.data[:, 1]
-
-    def calc_chi2(self):
-        """
-        Calculate the chi2 of the data relative to the model.
-
-        sets self.chi2 = Sum_k (residuals[k]^2) / data[k, 2]^2
-        """
-        if self.residuals is None:
-            self.calc_residuals()
-
-        self._chi2 = np.sum(self.residuals ** 2 / self.data[:, 2] ** 2)
-
-    def calc_df(self):
-        """
-        FUNCTION SPECIFIC: Calculate the derivatives of the fitting
-        function relative to each parameter theta and store as
-        self.df.  Should be explicitly defined for a specific class
-        that inherits SFitFunction.
-
-        sets self.df =
-            *np.array* of shape (M, N) where df[i, k] returns the derivative of the fitting function with respect to
-            parameter i evaluated at point k.
-
-        """
-        raise NotImplementedError('User must define calc_model() for a child class of SFitFunction.')
-
-    def calc_dchi2(self):
-        """Calculate the gradient of chi2 at each datapoint"""
-
-        if self.df is None:
-            self.calc_df()
-
-        if self.residuals is None:
-            self.calc_residuals()
-
-        chi2_gradient = []
-        for i in range(len(self.theta)):
-            chi2_gradient.append(
-                -2 * self.residuals * self.df[i] / self.data[:, 2]**2)
-
-        self._dchi2 = np.array(chi2_gradient)
-
-    def calc_dvec(self):
-        """
-        Calculate the d vector and store it as self.dvec.
-        d_i = d_i + (partial chi2/partial a_i)/2
-        """
-        if self.dchi2 is None:
-            self.calc_dchi2()
-
-        self._dvec = np.sum(self.dchi2, axis=1) / 2.
-
-    def calc_bmat(self):
-        """
-        Calculate the b matrix and store it as self.bmat.
-        b_ij = b_ij + (partial F/partial a_i)(partial F/partial a_j)/error^2
-        """
-        if self.df is None:
-            self.calc_df()
-
-        self._bmat = np.inner(self.df, self.df / self.data[:, 2]**2)
-        # self._bmat = np.zeros((len(self.theta), len(self.theta)))
-        # for i in range(len(self.theta)):
-        #     for j in range(len(self.theta)):
-        #         self._bmat[i, j] = np.sum(
-        #             self.df[i] * self.df[j] / self.data[:, 2]**2)
-
-    def calc_cmat(self):
-        """Invert the b matrix to find the c matrix."""
-        if self.bmat is None:
-            self.calc_bmat()
-
-        self._cmat = np.linalg.inv(self.bmat)
-
-    def calc_step(self):
-        """
-        Calculate the full proposed step size.
-        Delta a_i = sum_j c_ij * d_j.
-        """
-        if self.cmat is None:
-            self.calc_cmat()
-
-        if self.dvec is None:
-            self.calc_dvec()
-
-        self._step = np.sum(self.cmat * self.dvec, axis=1)
-        # self._step = np.zeros(len(self.theta))
-        # for i in range(len(self.theta)):
-        #     for j in range(len(self.theta)):
-        #         self._step[i] += self.cmat[i, j] * self.dvec[j]
-
-    def calc_sigmas(self):
-        """
-        Calculate the uncertainty in each parameter theta.
-        sigma_a_i = sqrt(c_ii)
-        """
-        if self.cmat is None:
-            self.calc_cmat()
-
-        # self._sigmas = np.zeros(len(self.theta))
-        # for i in range(len(self.theta)):
-        #     self._sigmas[i] = np.sqrt(self.cmat[i, i])
-
-        self._sigmas = np.sqrt(np.diagonal(self.cmat))
-
-    # Properties
-    @property
-    def theta(self):
-        """
-        *list* or *np.array*
-
-        Parameters of the function being fit to the data.
-        """
-        return self._theta
-
-    @theta.setter
-    def theta(self, value):
-        self._reset_all()
-        if value is None:
-            self._theta = value
-        else:
-            if isinstance(value, (list, np.ndarray)):
-                self._theta = value
-            else:
-                raise TypeError(
-                    'theta must be either list or np.array. Type: {0}'.format(type(value))
-                )
-
+    # Model Values
     @property
     def ymod(self):
         """
-        *np.array* of shape (N)
-
-        model values for data[:, 0].
+        *np.array* of shape (N), where each element k is the value of the model evaluated at data[k, 0].
         """
         return self._ymod
 
@@ -310,12 +193,22 @@ class SFitFunction(object):
         else:
             raise TypeError('ymod must be an np.array object. Type: {0}'.format(type(value)))
 
+    def calc_model(self):
+        """
+        FUNCTION SPECIFIC: Calculate expected values of the model. May
+        be explicitly defined for a specific class that inherits
+        SFitFunction.
+
+        sets :py:attr:`~ymod`
+
+        """
+        raise NotImplementedError('User must define calc_model() for a child class of SFitFunction.')
+
+    # Residuals
     @property
     def residuals(self):
         """
-        *np.array* of shape (N)
-
-        residuals of the model from the data: residuals = y - ymod
+        *np.array* of shape (N), where each element k = :py:attr:`~ymod`[k] - data[k, 1].
         """
         return self._residuals
 
@@ -331,19 +224,54 @@ class SFitFunction(object):
         else:
             raise TypeError('residuals must be an np.array object. Type: {0}'.format(type(value)))
 
+    def calc_residuals(self):
+        """
+        Difference between the data and the model. Either this or
+        calc_model() should be explicitly defined for a specific
+        class that inherits SFitFunction.
+
+        sets :py:attr:`~residuals`
+        """
+        if self.ymod is None:
+            self.calc_model()
+
+        self.residuals = self.ymod - self.data[:, 1]
+
+    # Chi2
     @property
     def chi2(self):
-        """ chi2 of the model with respect to the data"""
+        """
+        *float*
+
+        chi2 of the model with respect to the data.
+        chi2 = Sum_k (residuals[k]^2) / data[k, 2]^2
+        """
         return self._chi2
 
+    def calc_chi2(self):
+        """
+        Calculate the chi2 of the data relative to the model.
+
+        sets :py:attr:`~chi2`
+        """
+        if self.residuals is None:
+            self.calc_residuals()
+
+        self._chi2 = np.sum(self.residuals ** 2 / self.data[:, 2] ** 2)
+
+    def get_chi2(self):
+        """Calculate and return the chi2 of the data w.r.t. the model. See :py:func:`~calc_chi2`."""
+        self.calc_chi2()
+
+        return self.chi2
+
+    # df
     @property
     def df(self):
         """
-        *np.array* of shape (M, N)
-
-        Partial derivatives of the *fitted function F* with respect
-        to the parameters, calculated at each data point.
-        shape = (len(theta), len(data))"""
+        *np.array* of shape (M, N), where df[i, k] returns the partial derivative of the *fitting function* with respect
+        to parameter i evaluated at point k. shape = (len(theta), len(data))
+        """
         if self._df is None:
             self.calc_df()
 
@@ -362,84 +290,187 @@ class SFitFunction(object):
         else:
             raise TypeError('df must be an np.array object. Type: {0}'.format(type(value)))
 
-    @property
-    def dchi2(self):
-        """Partial derivatives of the *chi2* with respect
-        to the parameters, calculated at each data point.
-        shape = (len(theta), len(data))"""
-        return self._dchi2
-
-    @property
-    def dvec(self):
+    def calc_df(self):
         """
-        d vector from sfit, used to calculate the step size.
-        shape = ( len(theta) )
-        """
-        return self._dvec
+        FUNCTION SPECIFIC: Calculate the derivatives of the fitting
+        function relative to each parameter theta and store as
+        self.df.  Should be explicitly defined for a specific class
+        that inherits SFitFunction.
 
-    @property
-    def bmat(self):
+        sets :py:attr:`~df`
         """
-        b matrix from sfit. invert to find c matrix used for stepping and
-        finding sigmas.
-        shape = ( len(theta), len(theta) )
-        """
-        return self._bmat
+        raise NotImplementedError('User must define calc_model() for a child class of SFitFunction.')
 
-    @property
-    def cmat(self):
-        """c matrix from sfit. shape = ( len(theta), len(theta) )"""
-        return self._cmat
-
+    # Sigmas
     @property
     def sigmas(self):
-        """Uncertainty in each parameter theta"""
+        """
+        *np.array* of length M
+
+        Uncertainty in each parameter theta: sigma_theta_i = sqrt(c_ii)"""
         return self._sigmas
 
+    def calc_sigmas(self):
+        """
+        Calculate the uncertainty in each parameter :py:attr:`~theta`.
+        """
+        if self.cmat is None:
+            self.calc_cmat()
+
+        self._sigmas = np.sqrt(np.diagonal(self.cmat))
+
+    def get_sigmas(self):
+        """Calculate and return the uncertainty in each parameter theta. see :py:func:`~calc_sigmas`"""
+        self.calc_sigmas()
+
+        return self.sigmas
+
+    # Step
     @property
     def step(self):
-        """Step for each parameter theta"""
+        """
+        *np.array* of size (M)
+
+        Full proposed step size for each parameter theta_i:
+            theta_new_i = theta_old_i + step_i where
+            step_i = sum_j c_ij * d_j.
+        """
         return self._step
 
-    # Retrieval Functions
-    def get_chi2(self):
-        """Calculate and return the chi2 of the data w.r.t. the model"""
-        self.calc_chi2()
+    def calc_step(self):
+        """
+        Calculate :py:attr:`~step`.
+        """
+        if self.cmat is None:
+            self.calc_cmat()
 
-        return self.chi2
+        if self.dvec is None:
+            self.calc_dvec()
 
-    def get_partials(self):
-        """Calculate and return the partial derivatives of the fitting function"""
-        self.calc_df()
-
-        return np.sum(self.df, axis=1)
-
-    def get_dvec(self):
-        """Calculate and return the d vector"""
-        self.calc_dvec()
-
-        return self.dvec
-
-    def get_bmat(self):
-        """Calculate and return the b matrix"""
-        self.calc_bmat()
-
-        return self.bmat
-
-    def get_cmat(self):
-        """Calculate and return the c matrix"""
-        self.calc_cmat()
-
-        return self.cmat
+        self._step = np.sum(self.cmat * self.dvec, axis=1)
 
     def get_step(self):
-        """Calculate and return the full step for theta_new = theta_old + step."""
+        """Calculate and return the full step for each parameter theta_i. See :py:func:`~calc_step`."""
         self.calc_step()
 
         return self.step
 
-    def get_sigmas(self):
-        """Calculate and return the uncertainty in each parameter theta."""
-        self.calc_sigmas()
+    # dchi2 (chi2 gradient)
+    @property
+    def dchi2(self):
+        """
+        *np.array* of shape (M, N)
 
-        return self.sigmas
+        Partial derivatives of the *chi2* with respect
+        to the parameters, calculated at each data point.
+        shape = (len(theta), len(data))
+        """
+        return self._dchi2
+
+    def calc_dchi2(self):
+        """Calculate the gradient of chi2 (:py:attr:`~dchi2`) at each datapoint."""
+
+        if self.df is None:
+            self.calc_df()
+
+        if self.residuals is None:
+            self.calc_residuals()
+
+        chi2_gradient = []
+        for i in range(len(self.theta)):
+            chi2_gradient.append(
+                -2 * self.residuals * self.df[i] / self.data[:, 2] ** 2)
+
+        self._dchi2 = np.array(chi2_gradient)
+
+    def get_dchi2(self):
+        """Calculate and return the partial derivatives of the fitting function. See :py:func:`~calc_dchi2`."""
+        self.calc_df()
+
+        return np.sum(self.df, axis=1)
+
+    def get_partials(self):
+        """Alternative to :py:func:`~get_dchi2`."""
+        return self.get_dchi2()
+
+    # d vector
+    @property
+    def dvec(self):
+        """
+        *np.array* of size (M)
+
+        d vector from sfit, used to calculate the :py:attr:`~step` size:
+            d_i = Sum_k (partial chi2 / partial theta_i) / 2
+
+        shape = ( len(theta) )
+        """
+        return self._dvec
+
+    def calc_dvec(self):
+        """
+        Calculate the d vector and store it as :py:attr:`~dvec`.
+        """
+        if self.dchi2 is None:
+            self.calc_dchi2()
+
+        self._dvec = np.sum(self.dchi2, axis=1) / 2.
+
+    def get_dvec(self):
+        """Calculate and return the d vector. See :py:func:`~calc_dvec`."""
+        self.calc_dvec()
+
+        return self.dvec
+
+    # b matrix
+    @property
+    def bmat(self):
+        """
+        *np.array* of size (M, M)
+
+        b matrix from sfit. invert to find c matrix (:py:attr:`~cmat`) used for stepping and
+        finding sigmas:
+            b_ij = Sum_k (partial F / partial theta_i)(partial F/partial theta_j) / data[k, 2]^2
+
+        where F is the *fitting function* and data[k, 2] are the errors. shape = ( len(theta), len(theta) ).
+        """
+        return self._bmat
+
+    def calc_bmat(self):
+        """
+        Calculate the b matrix (:py:attr:`~bmat`).
+        """
+        if self.df is None:
+            self.calc_df()
+
+        self._bmat = np.inner(self.df, self.df / self.data[:, 2]**2)
+
+    def get_bmat(self):
+        """Calculate and return the b matrix. See :py:func:`~calc_bmat`."""
+        self.calc_bmat()
+
+        return self.bmat
+
+    # c matrix
+    @property
+    def cmat(self):
+        """
+        *np.array* of size (M, M)
+
+        c matrix from sfit:
+            c = inv(:py:attr:`~bmat`)
+
+        shape = ( len(theta), len(theta) )"""
+        return self._cmat
+
+    def calc_cmat(self):
+        """Invert the b matrix (:py:attr:`~bmat`) to find the c matrix (:py:attr:`~cmat`)."""
+        if self.bmat is None:
+            self.calc_bmat()
+
+        self._cmat = np.linalg.inv(self.bmat)
+
+    def get_cmat(self):
+        """Calculate and return the c matrix. See :py:func:`~calc_cmat`."""
+        self.calc_cmat()
+
+        return self.cmat
